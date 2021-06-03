5d402d1059d012fca369024f3b841861d178ede9
igoreta <ivoronline@gmail.com>
2021-06-03 13:24:54 +0200

    Initial commit

---
+++ src/main/java/com/ivoronline/springboot_deserialize_xml_annotations_properties/SpringbootDeserializeXmlAnnotationsPropertiesApplication.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_deserialize_xml_annotations_properties;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootDeserializeXmlAnnotationsPropertiesApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringbootDeserializeXmlAnnotationsPropertiesApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/ivoronline/springboot_deserialize_xml_annotations_properties/SpringbootDeserializeXmlAnnotationsPropertiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivoronline.springboot_deserialize_xml_annotations_properties;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDeserializeXmlAnnotationsPropertiesApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
