602920046000667cc61d89672913540012573f70
igoreta <ivoronline@gmail.com>
2021-11-17 22:53:40 +0100

    Commit

--- src/main/java/com/ivoronline/springboot_deserialize_xml_annotations_properties/dto/PersonDTO.java
+++ src/main/java/com/ivoronline/springboot_deserialize_xml_annotations_properties/dto/PersonDTO.java
@@ -5,7 +5,7 @@ import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import javax.xml.bind.annotation.XmlTransient;
 
-@XmlRootElement
+@XmlRootElement(name = "person")
 public class PersonDTO {
 
   //PROPERTIES
